bd7d1470ee6be7fa1f8eac31648c561443f4bf78
Biraj Thapa <birajthapa983@gmail.com>
2024-05-18 13:42:03 +0545

    added footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <Footer/>
     </>
   );
 };
