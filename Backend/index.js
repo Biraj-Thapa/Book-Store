668bb36c8ce87ac5dca8ae340948c6fd5c7c1cc0
Biraj Thapa <birajthapa983@gmail.com>
2024-05-23 17:40:27 +0545

    backend Setup

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv=require("dotenv")
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
