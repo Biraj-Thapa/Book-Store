7b07be240fecb3d845862e355c6a96dc0d0d80b8
Biraj Thapa <birajthapa983@gmail.com>
2024-05-24 17:07:32 +0545

    added backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,33 @@
-const express = require('express')
-const dotenv=require("dotenv")
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import bookRoute from "./route/book.route.js";
+
 const app = express()
 dotenv.config()
 const port = process.env.PORT || 5000
+const connection=async()=>{
+    try{
+      const isConnected = await mongoose.connect('mongodb://127.0.0.1:27017/kitabSansar');
+     if(isConnected){
+       console.log("connected  to mongodb")
+    }
+}catch(err) {
+ console.log(err)
+}
+connection();
 
-app.get('/', (req, res) => {
+}
+
+app.use("/book", bookRoute);
+
+
+
+
+app.get('/', (req, res) => 
   res.send('Hello World!')
-})
+)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
