a6abb4bd38face2bfca80500b60c08db9e1e6e6d
Biraj Thapa <birajthapa983@gmail.com>
2024-06-08 21:32:19 +0545

    user backend completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
 
 import bookRoute from "./route/book.route.js";
-
+import userRoute from "./route/user.route.js";
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 dotenv.config();
 const port = process.env.PORT || 5000;
 const connection = async () => {
@@ -20,11 +21,12 @@ const connection = async () => {
     }
   } catch (err) {
     console.log(err);
-  }  
+  }
 };
 connection();
 
 app.use("/book", bookRoute);
+app.use("/user", userRoute);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
